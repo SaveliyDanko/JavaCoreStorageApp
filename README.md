2a660295ed5532d1165fc86f4a09c3452000ecd6
Saveliy Danko <saveliydanko.m@gmail.com>
2024-05-22 15:21:03 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A multithreaded client-server application that interacts with a PostgresSQL database
---
+++ Server/src/main/java/com/savadanko/server/ServerApp.java
@@ -0,0 +1,7 @@
+package com.savadanko.server;
+
+public class ServerApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
